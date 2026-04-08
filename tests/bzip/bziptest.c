0a271e9bae5d306b69d3b450edb26b124c89d563
crocsg <pub.sg@free.fr>
2026-04-08 14:12:47 +0200

    dev:add test

---
+++ tests/bzip/bziptest.c
@@ -0,0 +1,15 @@
+#include "loudatabase.h"
+
+extern lou_dbfile lou_database[];
+
+#define ARRAYSIZE(a) (sizeof(a)/sizeof((a[0])))
+
+
+int main (void)
+{
+    for (int i = 0;i < ARRAYSIZE(lou_database);i++)
+    {
+        printf ("filename : %s\n",lou_database[i].fname);
+    }
+    return (0);
+}
